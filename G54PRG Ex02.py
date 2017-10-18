fbc13ad41dbf061211724f24ff81199d61bb81d7
cyw500 <cyw500@gmail.com>
2017-10-19 00:12:16 +0100

    2nd

--- G54PRG Ex02.py
+++ G54PRG Ex02.py
@@ -1,7 +1,7 @@
 "Programming with Python (G54PRG) 2nd exercise"
-low = 1
-high = 100
-print("Think of a number between {} and {}!".format(low,high))
+x = low = 1
+y = high = 100
+print("Think of a number between {} and {}!".format(x,y))
 count = 0
 Gotit = False
 while not Gotit:
@@ -18,8 +18,20 @@ while not Gotit:
         high = guess
     elif answer == ">": 
         low = guess
-    else: # when user input did is not ">" or "=" or "<"
+    else: # when user input is not ">" or "=" or "<"
         count -= 1
         print ( "Please answer with a single symbol of < or = or > only!")
+    if low == high :
+        print ("You must be thinking outside the range, the number must between {} and {}!".format(x,y))
+
+        ''' 
+        thinking of asking do you want to try again but then I dont know how to feed it back to the loop again 
+        (also if not how to not showing the "I needed ? steps!", I have consider putting if statement if count > 6 
+        but it is possible to have 7 step to guess as well as it fall out of range)
+        '''
+
+        break
+    elif guess == high - 1 :
+        low += 1
 
 print("I needed {} steps!".format(count))
