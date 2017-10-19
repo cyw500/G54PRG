e97e08e759c86dbd594a8f728962774d22bc5692
cyw500 <cyw500@gmail.com>
2017-10-19 14:17:21 +0100

    3rd
    Flaw in while not at edge of the range is there is one difference it bounce and print the guess again as well as the end message screw up when the guess is at top end

--- G54PRG Ex02.py
+++ G54PRG Ex02.py
@@ -1,37 +1,38 @@
 "Programming with Python (G54PRG) 2nd exercise"
-x = low = 1
-y = high = 100
+x = low = -10
+y = high = 10
 print("Think of a number between {} and {}!".format(x,y))
 count = 0
-Gotit = False
-while not Gotit:
-    guess =(low+high)//2
+gotit = False
+while not gotit:
+    guess = (low + high) // 2
 
-    print ("Is your number greater (>), equal (=), or less (<) than {}?".format(guess))    
+    print("Is your number greater (>), equal (=), or less (<) than {}?".format(guess))
     answer = input("Please answer <,=, or >! ")
    
     count += 1
     if answer == "=":
-        print ("I have guessed it!")
-        Gotit = True
+        print("I have guessed it!")
+        gotit = True
     elif answer == "<":
         high = guess
     elif answer == ">": 
         low = guess
     else: # when user input is not ">" or "=" or "<"
         count -= 1
-        print ( "Please answer with a single symbol of < or = or > only!")
-    if low == high :
-        print ("You must be thinking outside the range, the number must between {} and {}!".format(x,y))
+        print("Please answer with a single symbol of < or = or > only!")
 
-        ''' 
-        thinking of asking do you want to try again but then I dont know how to feed it back to the loop again 
-        (also if not how to not showing the "I needed ? steps!", I have consider putting if statement if count > 6 
-        but it is possible to have 7 step to guess as well as it fall out of range)
-        '''
+    end = "I needed {} steps!".format(count)
 
-        break
-    elif guess == high - 1 :
+    if low == high:
+
+        if low == x or high == y:
+            end = "You must be thinking outside the range, the number must between {} and {}!".format(x, y)
+            break
+        else:
+            end = "What are you thinking? It is need to be a whole number!"
+            break
+    if guess != y and guess == (high - 1):
         low += 1
 
-print("I needed {} steps!".format(count))
+print(end)
