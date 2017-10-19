55d37654efa318891ea26308ce3772032a7a80e1
cyw500 <cyw500@gmail.com>
2017-10-19 22:03:54 +0100

    Update G54PRG Ex02.py

--- G54PRG Ex02.py
+++ G54PRG Ex02.py
@@ -1,12 +1,12 @@
 "Programming with Python (G54PRG) 2nd exercise"
 bottom = low = 1
 top = high = 100
-<<<<<<< HEAD
+
 n = math.log(top - bottom , 2)
 print("OK I will show you a magic that I can guess what you thinking in less then {} steps if you thinking of a number between {} and {}!".format(math.ceil(n)+1, bottom,top))
-=======
+
 print("Think of a number between {} and {}!".format(bottom,top))
->>>>>>> parent of 2183a77... 1st
+
 count = 0
 while True:
     guess = (low + high) // 2
@@ -15,7 +15,7 @@ while True:
     answer = input("Please answer <,=, or >! ")
    
     count += 1
-    end = "I needed {} steps!".format(count)
+    end = "See , I needed {} steps!".format(count)
 
     if answer == "=":
         print("I have guessed it!")
@@ -34,14 +34,14 @@ while True:
             low = guess
     else: # when user input is not ">" or "=" or "<"
         count -= 1
-        print("Please answer with a single symbol of < or = or > only!")
+        print("Are you blind! < or = or > only!")
     #    continue
 
     if low == high:
-        end = "you must be thinking outside the range, the number must between {} and {}!".format(bottom , top)
+        end = "How thick can you be! {} to {} !".format(bottom,top) 
         break
     if (high - low) == 1:
-        end = "What are you thinking? It is need to be a whole number!"
+        end = "Your brain must have holes!!!"
         break
 
 print(end)
